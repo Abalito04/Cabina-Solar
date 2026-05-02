962d2898d1fe5e296726bbcf5350e018cb14c375
Matias Abalo <abalito95@gmail.com>
2026-05-01 21:41:43 -0300

    fix: notificacion desde SW para alarma sin gesto de usuario en PWA

--- app/static/service-worker.js
+++ app/static/service-worker.js
@@ -1,2 +1,32 @@
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => clients.claim());
+
+// Escucha mensajes desde la pagina para mostrar notificacion con sonido
+self.addEventListener('message', e => {
+    if (e.data && e.data.tipo === 'alarma') {
+        self.registration.showNotification('\u23F1 Cabina Solar', {
+            body: e.data.nombre
+                ? '\u00A1La sesi\u00F3n de ' + e.data.nombre + ' ha terminado!'
+                : '\u00A1La sesi\u00F3n ha terminado!',
+            icon: '/static/icons/icon-192.png',
+            badge: '/static/icons/icon-192.png',
+            vibrate: [500, 300, 500, 300, 500],
+            tag: 'alarma-cabina',
+            renotify: true,
+            requireInteraction: true
+        });
+    }
+});
+
+// Click en la notificacion: enfocar la PWA
+self.addEventListener('notificationclick', e => {
+    e.notification.close();
+    e.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(lista => {
+            if (lista.length > 0) {
+                return lista[0].focus();
+            }
+            return clients.openWindow('/');
+        })
+    );
+});
